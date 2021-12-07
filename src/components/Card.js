0364447ccd1eb62cd7c1945a5e3ba64a8a176770
PracticumStudent <idanx061@gmail.com>
2021-12-07 09:33:25 +0200

    small fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,10 @@ import React from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 function Card(props) {
 
-    const { cardClickHandler, onCardDelete, card, key, onCardLike} = props;
+    const { cardClickHandler, onCardDelete, card, onCardLike} = props;
     const currentUser = React.useContext(CurrentUserContext);
 
-    
     const isOwn = card.owner._id === currentUser._id;
-
     // Creating a variable which you'll then set in `className` for the delete button
     const cardDeleteButtonClassName = (
         `elements__delete-button ${isOwn ? 'elements__delete-button_active' : 'elements__delete-button_hidden'}`
@@ -20,7 +18,7 @@ function Card(props) {
     const cardLikeButtonClassName = `elements__like-button ${isLiked ? 'elements__like-button_active' : 'elements__like-button_hidden'}`;
 
     return (
-        <li className="elements__card" key={key}>
+        <li className="elements__card">
             {isOwn && (
             <button type="button" onClick={() => onCardDelete(card)} className={`${cardDeleteButtonClassName}`} aria-label="delete button"></button>
             )}
@@ -29,7 +27,9 @@ function Card(props) {
                 <h2 className="elements__title">{card.name}</h2>
                 <div className="elements__likes-container">
                     <button onClick={() => onCardLike(card)} type="button" className={`${cardLikeButtonClassName}`} aria-label="like button"></button>
-                    <p className="element__like-counter"></p>
+                    <p className="element__like-counter">
+                        {card.likes.length}
+                    </p>
                 </div>
             </div>
         </li>
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 function ImagePopup(props) {
-    const { cardData, onClose, isOpen } = props;
+    const { onClose, isOpen, cardObj } = props;
     return (
         <div className={`popup popup_theme_open-photo ${isOpen && 'popup_open'}`}>
             <div className="popup__container popup__container_theme_open-photo">
                 <button type="button" className="popup__close-button" onClick={onClose} aria-label="close button"></button>
                 <figure>
-                    <img className="popup__open-image" alt="" src={cardData.link} />
-                    <figcaption className="popup__open-image-caption">{cardData.name}</figcaption>
+                    <img className="popup__open-image" alt={!cardObj ? null : cardObj.name} src={cardObj.link} />
+                    <figcaption className="popup__open-image-caption">{cardObj.name}</figcaption>
                 </figure>
             </div>
         </div>
